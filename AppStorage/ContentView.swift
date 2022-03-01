7688ff0638a7b1d7830e8addb22f5eb4980813c0
Federico <codepalace@fastmail.se>
2022-03-01 10:36:14 +0100

    First commit

--- AppStorage/ContentView.swift
+++ AppStorage/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+   @AppStorage("STRING_KEY") var savedText: String = ""
+   @AppStorage("NUMBER_KEY") var counter: Int = 0
+   @State private var text = ""
+   
+   var body: some View {
+      VStack {
+         Text("\(counter)")
+            .font(.system(size: 100, weight: .bold, design: .serif))
+            .bold()
+            .foregroundColor(.red)
+            .onTapGesture {
+               counter += 1
+         }
+         
+         TextField("Type something", text: $text)
+            .onChange(of: text) { text in
+               self.savedText = text
+            }
+            .textFieldStyle(.roundedBorder)
+            .font(.title)
+            .disableAutocorrection(true)
+            .onAppear {
+               self.text = savedText
+               print("Loaded: \(savedText)")
+            }
+         Spacer()
+      }
+      .padding()
+   }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+   static var previews: some View {
+      ContentView()
+   }
 }
