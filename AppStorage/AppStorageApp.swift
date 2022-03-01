98efc766906aab2ecaed0944f605c84799e11f7e
Federico <codepalace@fastmail.se>
2022-03-01 09:48:21 +0100

    Initial Commit

---
+++ AppStorage/AppStorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStorageApp.swift
+//  AppStorage
+//
+//  Created by Federico on 01/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct AppStorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
